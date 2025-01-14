1feb6dc483783775de0d0b2292bb9bf18384dbc2
roumaissa <roumaissa8901@gmail.com>
2025-01-13 20:26:17 -1200

    ends of the checkpoint

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+  // select the color-box and change-color-btn elements
   const btn = document.getElementById("change-color-btn");
   const box = document.getElementById("color-box");
   //   this fucntion return an hexadecimal number
@@ -6,8 +7,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
     return randomColor;
   }
-   //   this is function with an RBG color
-   function getRandomColorRGB() {
+  //   this is function with an RBG color
+  function getRandomColorRGB() {
     const randomColorRGB = `rgb(${Math.floor(
       Math.random() * 255
     )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
