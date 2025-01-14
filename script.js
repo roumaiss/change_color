41f14389ec6a8fd0f85727343b393a6e23c45c56
roumaissa <roumaissa8901@gmail.com>
2025-01-13 20:21:14 -1200

    end of the checkpoint

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const btn = document.getElementById("change-color-btn");
+  const box = document.getElementById("color-box");
+  //   this fucntion return an hexadecimal number
+  function getRandomColor() {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+  }
+   //   this is function with an RBG color
+   function getRandomColorRGB() {
+    const randomColorRGB = `rgb(${Math.floor(
+      Math.random() * 255
+    )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(
+      Math.random() * 255
+    )})`;
+    return randomColorRGB;
+  }
+  btn.addEventListener("click", () => {
+    // using the hexadecimal color function
+    box.style.backgroundColor = getRandomColor();
+    // using the RGB color function
+    // box.style.backgroundColor = getRandomColorRGB();
+  });
+});
